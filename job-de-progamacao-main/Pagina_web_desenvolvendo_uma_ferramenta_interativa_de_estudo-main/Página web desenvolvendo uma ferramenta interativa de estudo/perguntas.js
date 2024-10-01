c4086fb45dfbd44cb5c06713659af55e6d329f73
jplive12o <163213133+jplive12o@users.noreply.github.com>
2024-10-01 07:53:18 -0300

    Add files via upload

---
+++ job-de-progamacao-main/Pagina_web_desenvolvendo_uma_ferramenta_interativa_de_estudo-main/Página web desenvolvendo uma ferramenta interativa de estudo/perguntas.js
@@ -0,0 +1,46 @@
+criaCartao( //cria o cartao 
+    'Programação', //tópico da pergunta
+    'O que é inteligencia artificial', //pergunta
+    'O Python é uma linguagem de programação' //resposta
+)
+
+criaCartao(//cria o cartao 
+    'Geografia', //tópico da pergunta
+    'Quem foi a primeira IA a ser criada? quando e por quem? qual era seu objetivo?', //pergunta
+    'A capital da França é Paris' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Programação', //tópico da pergunta
+    'qual é o impacto da intelingencia artificial, no mundo atual?(na economia? na arte? na educação?', //pergunta
+    'Uma função é um bloco de código que executa alguma tarefa' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Lingua inglesa', //tópico da pergunta
+    'O que são roteadores?', //pergunta
+    'Oi em ingles é HI (RAI)' //resposta
+)
+criaCartao( //cria o cartao 
+    'Programação', //tópico da pergunta
+    'Quando a internet foi criada?', //pergunta
+    'O Python é uma linguagem de programação' //resposta
+)
+
+criaCartao(//cria o cartao 
+    'Geografia', //tópico da pergunta
+    'O que são cabos de transmissão intercontinentais de internet qual é a sua  velocidade de transmissão?', //pergunta
+    'A capital da França é Paris' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Programação', //tópico da pergunta
+    'O que é uma função?', //pergunta
+    'Uma função é um bloco de código que executa alguma tarefa' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Lingua inglesa', //tópico da pergunta
+    'Como se diz oi em Inglês?', //pergunta
+    'Oi em ingles é HI (RAI)' //resposta
+)
